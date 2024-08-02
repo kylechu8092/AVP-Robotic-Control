ef82daa912b1bdf1c118fcce3fd7c6842bc71259
unknown <kc5548092@gmail.com>
2024-08-02 13:03:42 -0700

    Login UI and file structure

---
+++ LoginForm/LoginUI.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace LoginForm
+{
+    partial class LoginUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameTxtBox = new System.Windows.Forms.TextBox();
+            this.passwordTxtBox = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // usernameTxtBox
+            // 
+            this.usernameTxtBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.usernameTxtBox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTxtBox.Location = new System.Drawing.Point(643, 177);
+            this.usernameTxtBox.Name = "usernameTxtBox";
+            this.usernameTxtBox.Size = new System.Drawing.Size(168, 27);
+            this.usernameTxtBox.TabIndex = 2;
+            this.usernameTxtBox.Enter += new System.EventHandler(this.usernameTxtBox_Enter);
+            this.usernameTxtBox.Leave += new System.EventHandler(this.usernameTxtBox_Leave);
+            // 
+            // passwordTxtBox
+            // 
+            this.passwordTxtBox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTxtBox.Location = new System.Drawing.Point(643, 229);
+            this.passwordTxtBox.Name = "passwordTxtBox";
+            this.passwordTxtBox.Size = new System.Drawing.Size(168, 27);
+            this.passwordTxtBox.TabIndex = 3;
+            this.passwordTxtBox.Enter += new System.EventHandler(this.passwordTxtBox_Enter);
+            this.passwordTxtBox.Leave += new System.EventHandler(this.passwordTxtBox_Leave);
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.loginBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(185)))), ((int)(((byte)(255)))));
+            this.loginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginBtn.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginBtn.ForeColor = System.Drawing.Color.White;
+            this.loginBtn.Location = new System.Drawing.Point(671, 281);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(108, 40);
+            this.loginBtn.TabIndex = 4;
+            this.loginBtn.Text = "LOG IN";
+            this.loginBtn.UseVisualStyleBackColor = false;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.BackColor = System.Drawing.Color.White;
+            this.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitBtn.ForeColor = System.Drawing.Color.White;
+            this.exitBtn.Image = global::LoginForm.Properties.Resources.icons8_close_50;
+            this.exitBtn.Location = new System.Drawing.Point(811, 9);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(38, 29);
+            this.exitBtn.TabIndex = 5;
+            this.exitBtn.UseVisualStyleBackColor = false;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LoginForm.Properties.Resources.avptechnologyllc_logo;
+            this.pictureBox2.Location = new System.Drawing.Point(643, -3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(168, 158);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LoginForm.Properties.Resources.avp_picturejpg;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(599, 398);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(856, 375);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.passwordTxtBox);
+            this.Controls.Add(this.usernameTxtBox);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginUI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox usernameTxtBox;
+        private System.Windows.Forms.TextBox passwordTxtBox;
+        private System.Windows.Forms.Button loginBtn;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button exitBtn;
+    }
+}
---
+++ LoginForm/LoginUI.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class LoginUI : Form
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+            InitalizeFields();
+        }
+
+        public void InitalizeFields()
+        {
+            usernameTxtBox.Text = "Enter username";
+            passwordTxtBox.Text = "Enter password";
+            usernameTxtBox.ForeColor = Color.Gray;
+            passwordTxtBox.ForeColor = Color.Gray;
+        }
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void usernameTxtBox_Enter(object sender, EventArgs e)
+        {
+            if (usernameTxtBox.Text == "Enter username")
+            {
+                usernameTxtBox.Text = string.Empty;
+                usernameTxtBox.ForeColor = Color.Black;
+            }
+        }
+
+        private void usernameTxtBox_Leave(object sender, EventArgs e)
+        {
+            if (usernameTxtBox.Text == "")
+            {
+                usernameTxtBox.ForeColor = Color.Gray;
+                usernameTxtBox.Text = "Enter username";
+            }
+        }
+
+        private void passwordTxtBox_Enter(object sender, EventArgs e)
+        {
+            if (passwordTxtBox.Text == "Enter password")
+            {
+                passwordTxtBox.Text = string.Empty;
+                passwordTxtBox.ForeColor = Color.Black;
+                passwordTxtBox.PasswordChar = '*';
+            }
+        }
+
+        private void passwordTxtBox_Leave(object sender, EventArgs e)
+        {
+            if (passwordTxtBox.Text == "")
+            {
+                passwordTxtBox.ForeColor = Color.Gray;
+                passwordTxtBox.Text = "Enter password";
+                passwordTxtBox.PasswordChar = '\0';
+            }
+        }
+    }
+
+
+    
+
+    
+}
