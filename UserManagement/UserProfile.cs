64ec11b4d916481323346fda3dc41b4bfbc0f7b9
unknown <kc5548092@gmail.com>
2024-08-05 15:06:45 -0700

    Built main UI, business logic for authentication, DTO

---
+++ UserManagement/UserProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DatabaseLayer.Models;
+namespace UserManagement
+{
+    public class UserProfile : Profile
+    {
+        Mapper map;
+        public UserProfile() 
+        {
+            CreateMap<User, UserDTO>();
+        }
+        
+        public UserDTO createDTO(User u)
+        {
+            return map.Map<User, UserDTO>(u);
+        }
+    }
+}
